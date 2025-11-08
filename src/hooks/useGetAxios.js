70d0bd9bd2bb735c046d61bfa870b589f935dd3b
Jorge Montesinos <osmarmontes50@gmail.com>
2025-11-07 20:42:27 -0500

    avance

---
+++ src/hooks/useGetAxios.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+//una regla para crear custom Hooks es que la función debe tener el prefijo 'use'
+const useGetAxios = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  //obj:hacer una petición inicial de datos
+  useEffect(() => {
+    const requestData = async () => {
+      try {
+        const response = await axios.get(url);
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    requestData();
+    //
+  }, [url]);
+
+  return { data, error }
+};
+
+export default useGetAxios;
