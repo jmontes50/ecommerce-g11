f72eb664f4e448c5642c5bb746bc4d7d5a1ddc47
Jorge Montesinos <osmarmontes50@gmail.com>
2025-11-10 19:30:57 -0500

    added proyecto final

--- src/store/useCartStore.js
+++ src/store/useCartStore.js
@@ -10,6 +10,24 @@ const useCartStore = create(devtools((set) => ({
   addProduct: (product) => {
     set((state) => {
       console.log(state);
+
+      //1. si existe el producto en el cart
+      const indexExists = state.cart.findIndex((item) => item.id === product.id);
+
+      if(indexExists === -1){ //no existe es un producto nuevo, por el -1 retornado
+        return {
+          cart: [...state.cart, { ...product, cantidad:1 }]//al ser nuevo solo le agregamos la cantidad: 1
+        }
+      } else { //si es mayor que -1 significa que existe ya en cart
+        const temporalCart = [...state.cart]; //hacemos una copia del carrito
+        temporalCart[indexExists].cantidad++; //en base al indice del findIndex (indexExists) encontramos el prod, he aumentamos la cantidad en +1
+        return {
+          cart: temporalCart,
+        }
+      }
+
+
+
       //la función que este dentro de set necesita retornar el cambio que quiere realizar en el store
       return {
         cart: [...state.cart, product]
