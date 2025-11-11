9815996794741e8aa61d5fecb83fe4d9ee27d463
Jorge Montesinos <osmarmontes50@gmail.com>
2025-11-10 21:52:40 -0500

    avance

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -24,6 +24,22 @@ const useAuthStore = create((set) => ({
     } catch (error) {
 
     }
+  },
+  loginUser: async (email, password) => {
+    try {
+      const response = await axios.post("https://simple-api-2ivd.onrender.com/auth/login", { email, password });
+      if(response.status === 200) {
+        const { token, usuario } = response.data;
+        toast.success("Bienvenido!");
+        //aquí no necesitamos acceder la información del state, así que podemos invocar directamente a set y darle en un objeto cuál es el cambio en el estado
+        set({ user: usuario, token: token, isLogged: true })
+        return true;
+      }else {
+        throw new Error("Error al ingresar, intente de nuevo")
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 }))
 
